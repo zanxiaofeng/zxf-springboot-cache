397bcd66b4427620830d0fe115a1e091345dbefb
Davis <zanxiaofeng@163.com>
2025-10-29 20:29:06 +0800

    update

---
+++ src/main/java/zxf/springboot/cache/rest/NetworkController.java
@@ -0,0 +1,39 @@
+package zxf.springboot.cache.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Arrays;
+
+@RestController
+@RequestMapping("/network")
+public class NetworkController {
+    @GetMapping("dns")
+    public ResponseEntity<String> dns(@RequestParam String host) {
+        try {
+            InetAddress[] addresses = InetAddress.getAllByName(host);
+            return ResponseEntity.ok(String.format("Host: %s, IPs: %s", host, Arrays.asList(addresses)));
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+            return ResponseEntity.status(404).body(String.format("Host not found: %s, error: ", host, ex));
+        }
+    }
+
+
+    @GetMapping("telnet")
+    public ResponseEntity<String> telnet(@RequestParam String host, @RequestParam int port) {
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress(host, port), 30 * 1000);
+            return ResponseEntity.ok("Successfully connected to " + host + ":" + port);
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+            return ResponseEntity.status(500).body("Failed to connect to " + host + ":" + port + ". Reason: " + ex.getMessage());
+        }
+    }
+}
