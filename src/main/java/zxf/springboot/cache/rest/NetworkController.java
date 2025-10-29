eb506a6b76555a3f2e521b93dfb67407af80987a
Davis <zanxiaofeng@163.com>
2025-10-29 20:39:12 +0800

    update

--- src/main/java/zxf/springboot/cache/rest/NetworkController.java
+++ src/main/java/zxf/springboot/cache/rest/NetworkController.java
@@ -10,6 +10,8 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.Arrays;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 
 @RestController
 @RequestMapping("/network")
@@ -18,10 +20,9 @@ public class NetworkController {
     public ResponseEntity<String> dns(@RequestParam String host) {
         try {
             InetAddress[] addresses = InetAddress.getAllByName(host);
-            return ResponseEntity.ok(String.format("Host: %s, IPs: %s", host, Arrays.asList(addresses)));
+            return ResponseEntity.ok(String.format("DNS resolution for %s: %s", host, Arrays.asList(addresses)));
         } catch (Exception ex) {
-            ex.printStackTrace(System.out);
-            return ResponseEntity.status(404).body(String.format("Host not found: %s, error: ", host, ex));
+            return ResponseEntity.status(404).body(String.format("DNS resolution error for %s: %s", host, ex.getMessage()));
         }
     }
 
@@ -34,6 +35,20 @@ public class NetworkController {
         } catch (Exception ex) {
             ex.printStackTrace(System.out);
             return ResponseEntity.status(500).body("Failed to connect to " + host + ":" + port + ". Reason: " + ex.getMessage());
+        }
+    }
+
+    @GetMapping("ssl")
+    public ResponseEntity<String> ssl(@RequestParam String host, @RequestParam int port) {
+        try {
+            SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+            try (SSLSocket socket = (SSLSocket) factory.createSocket(host, port)) {
+                socket.startHandshake();
+                return ResponseEntity.ok("Successfully established SSL connection to " + host + ":" + port);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+            return ResponseEntity.status(500).body("Failed to establish SSL connection to " + host + ":" + port + ". Reason: " + ex.getMessage());
         }
     }
 }
