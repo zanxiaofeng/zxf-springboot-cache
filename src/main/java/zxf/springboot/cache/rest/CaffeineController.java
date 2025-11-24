5a04d5853092289d1ff4df234e719cb955fcd8f0
Davis <zanxiaofeng@163.com>
2025-11-24 08:50:48 +0800

    update

--- src/main/java/zxf/springboot/cache/rest/CaffeineController.java
+++ src/main/java/zxf/springboot/cache/rest/CaffeineController.java
@@ -1,6 +1,5 @@
 package zxf.springboot.cache.rest;
 
-import com.github.benmanes.caffeine.cache.Cache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.CacheManager;
@@ -20,32 +19,19 @@ public class CaffeineController {
     private CacheManager employmentsCacheManager;
 
     @GetMapping("/caffeine/caches")
-    public Map<String, Collection<String>> caches() {
-        Map<String, Collection<String>> result = new HashMap<>();
-        result.put("addresses", addressesCacheManager.getCacheNames());
-        result.put("employments", employmentsCacheManager.getCacheNames());
+    public Map<String, Map<String, List<String>>> caches() {
+        Map<String, Map<String, List<String>>> result = new HashMap<>();
+        result.put("addresses", cacheInfo(addressesCacheManager));
+        result.put("employments", cacheInfo(employmentsCacheManager));
         return result;
     }
 
-    @GetMapping("/caffeine/addresses")
-    public Map<String, List<String>> addressesCache() {
-        Map<String, List<String>> result = new HashMap<>();
-        for (String cacheName : addressesCacheManager.getCacheNames()) {
-            CaffeineCache addressesCache = (CaffeineCache) addressesCacheManager.getCache(cacheName);
-            Cache cache = addressesCache.getNativeCache();
-            List<String> keys =  cache.asMap().keySet().stream().map(Object::toString).toList();
-            result.put(cacheName, keys);
-        }
-        return result;
-    }
 
-    @GetMapping("/caffeine/employments")
-    public Map<String, List<String>> employmentsCache() {
+    private Map<String, List<String>> cacheInfo(CacheManager cacheManager) {
         Map<String, List<String>> result = new HashMap<>();
-        for (String cacheName : employmentsCacheManager.getCacheNames()) {
-            CaffeineCache addressesCache = (CaffeineCache) employmentsCacheManager.getCache(cacheName);
-            Cache cache = addressesCache.getNativeCache();
-            List<String> keys =  cache.asMap().keySet().stream().map(Object::toString).toList();
+        for (String cacheName : cacheManager.getCacheNames()) {
+            CaffeineCache cache = (CaffeineCache) cacheManager.getCache(cacheName);
+            List<String> keys = cache.getNativeCache().asMap().keySet().stream().map(Object::toString).toList();
             result.put(cacheName, keys);
         }
         return result;
